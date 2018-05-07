7a74e296c8bc5b44f83e9fe31d57154bbe8c1be9
James King <j@lostwith.me>
2018-05-07 12:14:47 +0100

    get and convert cinemas

---
+++ scripts/cinemas.js
@@ -0,0 +1,18 @@
+var feed = require(__dirname + '/../things/feed');
+
+let convert = data => {
+  return data.cinemas.cinema.map(cinema => {
+    cinema.name = cinema.name.replace('Cineworld ', '');
+    delete cinema.root;
+
+    return cinema;
+  });
+};
+
+feed
+  .get('cinemas')
+  .then(convert)
+  .then(cinemas => feed.save('cinemas', cinemas))
+  .catch(err => {
+    console.log(err);
+  });
