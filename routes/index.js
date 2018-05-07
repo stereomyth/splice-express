5b68b7f4ad37fe56abe8f1a8ea5526bfddc075a6
James King <j@lostwith.me>
2018-05-07 12:27:24 +0100

    cinemas route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const feed = require(__dirname + '/../things/feed');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  feed
+    .query('cinemas')
+    .then(cinemas => {
+      res.send(cinemas);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
